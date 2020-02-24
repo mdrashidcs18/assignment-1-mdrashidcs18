0b01406a5b764b354814c454968cdd42f7f96863
Md Rashid <md.rashid_cs18@gla.ac.in>
2020-02-24 20:16:20 +0530

    Node Class Creation

---
+++ src/contact/Node.java
@@ -0,0 +1,23 @@
+package contact;
+
+public class Node<type> {
+    private type data;
+    private Node<type> next;
+
+    public Node(type data) {
+        this.data = data;
+    }
+
+    public type getData() {
+        return data;
+    }
+
+    public Node<type> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<type> next) {
+        this.next = next;
+    }
+}
+
